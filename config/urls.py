3864bfdfa03c03cc72981bab4ed59769c0e02ea4
MD Abdur Rahim <abdur.rahimcs50@gmail.com>
2026-08-21 07:43:58 +0600

    Customize Django admin site name/title

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "Fuel Route Optimizer Admin"
+admin.site.site_title = "Fuel Route Optimizer"
+admin.site.index_title = "Administration"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("routing.urls")),
